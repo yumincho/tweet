c6194a868f4d2560dbcfa517e6837297b4b84d7e
Yumin Cho <dbals081216@gmail.com>
2023-11-22 01:20:41 +0900

    impl: signup & login

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -5,19 +5,12 @@ const router = express.Router();
 
 const prisma = new PrismaClient();
 
-router.get("/dummy", async (req: any, res: any) => {
-  try {
-    /* todo */
-    res.send("dummy");
-  } catch (e) {
-    return res.status(500).json({ error: e });
-  }
-});
-
 router.post("/signup", async (req: any, res: any) => {
   try {
-    /* todo */
-    res.send("todo");
+    const { nickname, password } = req.body;
+    const result = await prisma.user.create({
+      data: { nickname, password },
+    });
   } catch (e) {
     return res.status(500).json({ error: e });
   }
@@ -25,8 +18,33 @@ router.post("/signup", async (req: any, res: any) => {
 
 router.post("/login", async (req: any, res: any) => {
   try {
-    /* todo */
-    res.send("todo");
+    const { nickname, password } = req.body;
+
+    const userList = await prisma.user.findMany({
+      select: {
+        nickname: true,
+        password: true,
+      },
+      where: {
+        nickname: nickname,
+      },
+    });
+
+    if (userList.length != 1) throw new Error("no unique user");
+
+    const user = userList[0];
+
+    if (user.nickname === nickname) {
+      if (user.password === password) {
+        console.log("login succeess");
+      } else {
+        console.log("password: WRONG");
+        throw new Error("password: WRONG");
+      }
+    } else {
+      console.log("nickname: NO DATA");
+      throw new Error("nickname: NO DATA");
+    }
   } catch (e) {
     return res.status(500).json({ error: e });
   }
