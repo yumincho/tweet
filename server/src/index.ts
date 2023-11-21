8c37c413d8dfa53ce3172b1ba6202f8ab46b3843
Yumin Cho <dbals081216@gmail.com>
2023-11-22 01:16:31 +0900

    add: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import authRouter from "./routes/auth";
 import tweetRouter from "./routes/tweet";
@@ -6,6 +7,20 @@ import commentRouter from "./routes/comment";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+
+const whitelist = ["http://localhost:5173"];
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    console.log("[REQUEST-CORS] Request from origin: ", origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true);
+    else callback(new Error("Not Allowed by CORS"));
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/auth", authRouter);
 app.use("/tweet", tweetRouter);
