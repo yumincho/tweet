8ad2658207ef38c22e58cdcf94c5f4514f099d21
Yumin Cho <dbals081216@gmail.com>
2023-11-28 00:29:48 +0900

    impl: textarea

--- client/src/components/widgets/Textarea.tsx
+++ client/src/components/widgets/Textarea.tsx
@@ -1,5 +1,42 @@
-const Textarea = () => {
-  return <textarea style={{ height: "80px" }} />;
+import React from "react";
+import axios from "axios";
+import { SAPIBase } from "../../tools/api";
+
+import "./Textarea.css";
+
+interface Props {
+  nickname: string;
+  tweetCount: number;
+  setTweetCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Textarea = ({ nickname, tweetCount, setTweetCount }: Props) => {
+  const [content, setContent] = React.useState("");
+
+  const addTweet = async () => {
+    await axios.post(SAPIBase + "/tweet", {
+      Id: tweetCount,
+      AuthorNickname: nickname,
+      Content: content,
+    });
+    setContent("");
+    setTweetCount(tweetCount + 1);
+  };
+  return (
+    <>
+      {/* <span className="tweetAuthor">작성자 | {nickname}</span>  */}
+      <div className="tweetContainer">
+        <textarea
+          className="tweetTextarea"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button className="tweetButton" onClick={addTweet}>
+          ↩︎
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Textarea;
