2f4314b8384c7fc3550f246a7a001f9a85a70d38
Yumin Cho <dbals081216@gmail.com>
2023-11-28 00:29:36 +0900

    fix: main page to pass right props

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,9 +1,23 @@
+import React from "react";
+import axios from "axios";
+import { SAPIBase } from "../tools/api";
+
 import Feed from "../components/Feed";
 import Textarea from "../components/widgets/Textarea";
 import UserInfo from "../components/UserInfo";
 import Calendar from "../components/Calendar";
 
 const MainPage = () => {
+  const [tweetCount, setTweetCount] = React.useState<number>(2);
+
+  const [nickname, setNickname] = React.useState("");
+  const userNickname = async () => {
+    const userNickname = await axios.get(SAPIBase + "/auth/nickname");
+    setNickname(userNickname.data);
+  };
+
+  userNickname();
+
   return (
     <>
       <div className="container test">
@@ -12,9 +26,13 @@ const MainPage = () => {
           <Calendar />
         </div>
 
-        <div className="rightContainer test">
-          <Feed />
-          <Textarea />
+        <div className="rightContainer">
+          <Feed tweetCount={tweetCount} />
+          <Textarea
+            nickname={nickname}
+            tweetCount={tweetCount}
+            setTweetCount={setTweetCount}
+          />
         </div>
       </div>
     </>
