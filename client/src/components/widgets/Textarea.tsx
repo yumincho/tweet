ac8cec65b4c5c21065e43eca20d74982be5360be
Yumin Cho <dbals081216@gmail.com>
2023-12-06 19:54:52 +0900

    :sparkles: tweet length lower limit

--- client/src/components/widgets/Textarea.tsx
+++ client/src/components/widgets/Textarea.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// import { Color } from "../styles";
 import "./Textarea.css";
 
 interface Props {
@@ -9,10 +10,10 @@ interface Props {
 }
 
 const Textarea = ({ content, setContent, addOne }: Props) => {
-  const [isValidLength, setIsValidLength] = React.useState(true);
+  const [isValidLength, setIsValidLength] = React.useState(false);
 
   const checkTweet = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (e.target.value.length > 140) {
+    if (e.target.value.length > 140 || e.target.value.length < 10) {
       setIsValidLength(false);
     } else {
       setIsValidLength(true);
@@ -23,6 +24,7 @@ const Textarea = ({ content, setContent, addOne }: Props) => {
       {/* <span className="tweetAuthor">작성자 | {nickname}</span>  */}
       <div className="tweetContainer">
         <textarea
+          placeholder="10자 이상, 140자 이하로 입력해 주세요."
           className={`tweetTextarea ${isValidLength ? "" : "error"}`}
           value={content}
           onChange={(e) => {
